f5f6993f51d18e845078a5c8b1b39bc29c1e6b5b
joakimgidlund <jockegidlund@gmail.com>
2025-05-18 22:01:43 +0200

    day classes

--- src/components/DayComponent.vue
+++ src/components/DayComponent.vue
@@ -34,32 +34,20 @@ export default {
 </script>
 
 <template>
-    <div v-if="percentage === 50" class="half-box" :class="[stat.stat1, stat.stat2, percentageString, activities]">
-        <div class="top"> {{ percentage }}%</div>
-        <div class="bottom"> {{ stat.stat1.slice(0, 4) }}. </div>
+    <div v-if="stat.stat1 != stat.stat2 && stat.stat2 != ''" class="day-box inter-four" :class="stat.stat1 + '-half'">
+        <div> {{ stat.stat1.slice(0, 4) }}. </div>
+        <div> {{ stat.stat2.slice(0, 4) }}. </div>
     </div>
-    <div v-else class="day-box" :class="[stat.stat1, stat.stat2, percentageString, activities]">
+    <div v-else-if="percentage === 50" class="day-box inter-four" :class="[stat.stat1, stat.stat2, percentageString, activities.act1]">
+        <div> {{ percentage }}%</div>
+        <div> {{ stat.stat1.slice(0, 4) }}. </div>
+    </div>
+    <div v-else-if="percentage === 100 || percentage === 0" class="day-box inter-four" :class="[stat.stat1, stat.stat2, percentageString]">
         <span v-if="stat === 'Preliminary'">Prel.</span><span v-if="stat.stat1 != 'Absent' ">{{ percentage }}%</span>
     </div>
 </template>
 
 <style scoped>
-.half-box {
-    width: 48px;
-    height: 63px;
-
-    display: flex;
-    flex-wrap: wrap;
-
-    justify-content: center;
-    align-items: center;
-    font-size: 17px;
-    font-weight: 400;
-
-    border-radius: 10px;
-    background: linear-gradient(#F6CA6C 50%, #FEE69B 50%);
-}
-
 .day-box {
     width: 48px;
     height: 63px;
@@ -70,8 +58,7 @@ export default {
     justify-content: center;
     align-items: center;
     font-size: 17px;
-    font-weight: 400;
-
+    
     border-radius: 10px;
     background-color: #ADE4C4;
 }
@@ -90,5 +77,13 @@ export default {
 
 .Preliminary {
     background-color: #F6CA6C;
+}
+
+.Preliminary-half {
+    background: linear-gradient(#F6CA6C 50%, #FEE69B 50%);
+}
+
+.Booked-half {
+    background: linear-gradient(#FEE69B 50%, #F6CA6C 50% );
 }
 </style>
